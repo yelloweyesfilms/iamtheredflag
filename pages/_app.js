b9f44c69534a3cc859af76c4a30335ba574be2d0
yelloweyesfilms <132815974+yelloweyesfilms@users.noreply.github.com>
2026-06-19 00:46:52 +0200

    GSC verification

--- pages/_app.js
+++ pages/_app.js
@@ -11,6 +11,7 @@ export default function App({ Component, pageProps }) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="apple-mobile-web-app-title" content="Red Flag" />
+<meta name="google-site-verification" content="p5h6HBJQhMyuOJtC-khMRAFJdJRZwu9bXGKnW_Aei4s" />
         <link rel="icon" href="/api/icon?size=512" />
         <link rel="apple-touch-icon" href="/api/icon?size=512" />
         <link rel="manifest" href="/manifest.json" />
