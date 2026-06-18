2a0304933c4f9f1b629491da78bb07b5dcc98786
yelloweyesfilms <132815974+yelloweyesfilms@users.noreply.github.com>
2026-06-19 00:59:30 +0200

    GA analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
@@ -11,11 +12,18 @@ export default function App({ Component, pageProps }) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black" />
         <meta name="apple-mobile-web-app-title" content="Red Flag" />
-<meta name="google-site-verification" content="p5h6HBJQhMyuOJtC-khMRAFJdJRZwu9bXGKnW_Aei4s" />
+        <meta name="google-site-verification" content="p5h6HBJQhMyuOJtC-khMRAFJdJRZwu9bXGKnW_Aei4s" />
         <link rel="icon" href="/api/icon?size=512" />
         <link rel="apple-touch-icon" href="/api/icon?size=512" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-F4Y5BTNDG0" strategy="afterInteractive" />
+      <Script id="ga-init" strategy="afterInteractive">{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-F4Y5BTNDG0');
+      `}</Script>
       <Component {...pageProps} />
     </>
   );
